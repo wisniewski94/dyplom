90d42b29c47c61bc9494c824619201244db927ef
Wiktor Wiśniewski <wiktorwisniewski@MacBook-Pro-Wiktor.local>
2020-03-14 15:28:44 +0100

    working canvas

--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -30,8 +30,8 @@ const Canvas = ({ width, height }) => {
     let maxIter = 100;
     for (let x = 10; x < height; x++)  {
         for (let y = 10; y < width; y++)  {
-          let m = mandelIter(x/mag - panX, y/mag - panY, maxIter);
-          //let m = await foo(x/mag - panX, y/mag - panY, maxIter);
+          //let m = mandelIter(x/mag - panX, y/mag - panY, maxIter);
+          let m = await foo(x/mag - panX, y/mag - panY, maxIter);
           ctx.fillStyle = (m === 0) ? '#000' : 'hsl(0, 100%, ' + m + '%)'; 
           ctx.fillRect(x, y, 1,1);
         }
@@ -39,8 +39,39 @@ const Canvas = ({ width, height }) => {
   }
   useEffect(() => {
     let testWASM;
-    asd();
-    
+    //asd();
+
+  (async() => {
+    const config = {
+        env: {
+            __memory_base: 0,
+            __table_base: 0,
+            memory: new WebAssembly.Memory({
+                initial: 256,
+            }),
+            table: new WebAssembly.Table({
+                initial: 0,
+                element: 'anyfunc',
+            }),
+        }
+      }
+      const fetchPromise = fetch(process.env.PUBLIC_URL + '/hello.wasm');
+      const {instance} = await WebAssembly.instantiateStreaming(fetchPromise, config);
+      testWASM = instance.exports.mandelIter;
+      let ctx = canvas.current.getContext('2d');
+    let mag = 200;
+    let panX = 2;
+    let panY = 1.25;
+    let maxIter = 100;
+    for (let x = 10; x < height; x++)  {
+        for (let y = 10; y < width; y++)  {
+          //let m = mandelIter(x/mag - panX, y/mag - panY, maxIter);
+          let m = testWASM(x/mag - panX, y/mag - panY, maxIter);
+          ctx.fillStyle = (m === 0) ? '#000' : 'hsl(0, 100%, ' + m + '%)'; 
+          ctx.fillRect(x, y, 1,1);
+        }
+      }
+  })();
                     
     });
   return <canvas ref={canvas} width={width} height={height} />;
